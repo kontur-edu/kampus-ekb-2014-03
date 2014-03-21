2c3791cc8093eabf26351798dec22b7e0f849da5
Konstantin Plotnikov <kost@hackerdom.ru>
2014-03-22 03:36:10 +0600

    Презентация и примеры по лекции Многопоточное и асинхронное программирование

---
+++ 05-multithreading/MultithreadingSamples/Sample2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample2
+{
+	class Program
+	{
+		static Stopwatch sw = new Stopwatch();
+
+		static void Main(string[] args)
+		{
+			
+			var tp = new MyThreadPool<long>(4);
+			sw.Start();
+			for(int i = 0; i < 1000; i++)
+			{
+				int i1 = i;
+				tp.Add(CheckPrime, 15485867 + i1);
+//				Task.Run(() => CheckPrime(15485867 + i1));
+//				CheckPrime(15485867 + i1);
+			}
+			Console.ReadLine();
+		}
+
+
+		private static void CheckPrime(long n)
+		{
+			for(int i = 2; i <= n / 2; i++)
+			{
+				if(n % i == 0)
+				{
+					Console.WriteLine("{2}	FALSE: {0} is complex, found divisor {1}", n, i, sw.ElapsedMilliseconds);
+					return;
+				}
+			}
+			Console.WriteLine("{1}	TRUE: {0} is prime", n, sw.ElapsedMilliseconds);
+		}
+	}
+}
---
+++ 05-multithreading/MultithreadingSamples/Sample3server/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sample3server
+{
+	class Program
+	{
+		private static HttpListener listener;
+
+		static void Main(string[] args)
+		{
+			listener = new HttpListener();
+			listener.Prefixes.Add("http://+:31337/data/");
+			listener.Start();
+
+			List<Timer> timers = new List<Timer>();
+
+			while(true)
+			{
+				try
+				{
+					var context = listener.GetContext();
+					Console.WriteLine("Accepted client {0}", context.Request.RemoteEndPoint);
+					timers.Add(new Timer(state =>
+					{
+						var ctx = (HttpListenerContext) state;
+						try
+						{
+							Console.WriteLine("Processing client {0}	{1}", context.Request.RemoteEndPoint, ctx.Request.RawUrl);
+							var bytes = Encoding.GetEncoding(1251).GetBytes(ctx.Request.RawUrl);
+							ctx.Response.OutputStream.Write(bytes, 0, bytes.Length);
+							Console.WriteLine("Written to client {0}	{1}", context.Request.RemoteEndPoint, ctx.Request.RawUrl);
+						}
+						finally
+						{
+							ctx.Response.Close();
+							Console.WriteLine("Processed client {0}	{1}", context.Request.RemoteEndPoint, ctx.Request.RawUrl);
+						}
+					}, context, 1000, Timeout.Infinite));
+				}
+				catch(Exception e)
+				{
+					Console.Error.WriteLine(e);
+				}
+			}
+		}
+	}
+}
